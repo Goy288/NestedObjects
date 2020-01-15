1266d2e227c377055ac9384827b8514776375739
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-01-15 14:13:19 -0800

    Creatd Course class.

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NestedObjects
+{
+    public class Course
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CourseID { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int CreditNumber { get; set; }
+    }
+}
