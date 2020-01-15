262d67fa8e8ebe680487d787b132d435be84c01b
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-01-15 14:19:08 -0800

    Added descriptions for the class properties, and renamed CreditNumber to Credits.

--- NestedObjects/Course.cs
+++ NestedObjects/Course.cs
@@ -7,23 +7,28 @@ namespace NestedObjects
     public class Course
     {
         /// <summary>
-        /// 
+        /// The course ID
         /// </summary>
         public int CourseID { get; set; }
 
         /// <summary>
-        /// 
+        /// The title for the course
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
-        /// 
+        /// Describes the course
         /// </summary>
         public string Description { get; set; }
 
         /// <summary>
-        /// 
+        /// The number of course credits
         /// </summary>
-        public int CreditNumber { get; set; }
+        public int Credits { get; set; }
+
+        /// <summary>
+        /// The number of course credits
+        /// </summary>
+        public List<Course> Schedule { get; set; }
     }
 }
